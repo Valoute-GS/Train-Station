d74fb65ccaf306a1d6668b1beaef03be50bd4257
15002787 <15002787@d026m08.istic.univ-rennes1.fr>
2018-11-19 17:09:54 +0100

    Station change works

--- Train Station/src/project/Station.java
+++ Train Station/src/project/Station.java
@@ -24,24 +24,31 @@ public class Station extends Thread {
 	}
 
 	// l'ajoute sur un quai si il y en a un de dispo
-	private void addTrainInPlatform (){
+	// quand un train arrive en sur le quai on le flush
+	private void managePlatform (){
 		for(int p = 0; p < capacity; p++){
+			// on ajoute un train au quai si possible
 			if(platforms[p] == null){
-				platforms[p] = queue.poll();
+				platforms[p] = queue.poll();	
+				
+			}
+			// On vide les trains a vider
+			if(platforms[p] != null && !platforms[p].flushed){
+				platforms[p].flush();;				
 			}
 		}
 
 	}
 	
-	
 	private void checkTrain(){
-		for(int p = 0; p < capacity; p++){
-			Train tmp = platforms[p];		
-			if(tmp != null && name == "A"){
-				if(tmp.freeSeats == 0){
+		for(int p = 0; p < capacity; p++){ //on check chaque platform
+			Train tmp = platforms[p];
+			if(tmp != null && tmp.flushed){
+				if(tmp.freeSeats == 0){ // si le train et plein il va a sa destination
 					tmp.destination.addTrainInQueue(tmp);
+					tmp.flushed = false;
 					platforms[p] = null;
-					
+										
 				}
 			}
 		}
@@ -63,12 +70,14 @@ public class Station extends Thread {
 	@Override
 	public void run(){
 		while(true){
-			sleep.millis(00);
-			// on verifie si un train n'attend pas son entree en gare
-			addTrainInPlatform();
+			sleep.millis(10);
+			
+			// REGROUPER LES OPERATIONS ET/OU LES FAIRE SEULEMENT SI NECESSAIRE POUR LIMITER LE NOMBRE D'APPELS
+			
+			// on verifie si un train n'attend pas son entree en gare et on vide les trains qui ont besoin
+			managePlatform();
 			// si un train est plein on le fait partir
 			checkTrain();
-			//jhfj
 			
 			
 		}
